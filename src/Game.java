b2b60f1028f41c67adcdbb20dea62c541d769ddc
Mc-StArT <alesha@gilenko.net>
2025-06-07 16:23:46 +0300

    Small refactor of Game class

--- src/Game.java
+++ src/Game.java
@@ -8,17 +8,16 @@ import org.json.simple.JSONObject;
 
 // Основной класс игры
 public class Game {
+    public static ArrayList<Integer> myHeroes;
     public static ArrayList<GameObject> gameObjects = new ArrayList<>();
+
+    static UI ui; // General ui handler
+
     public static void main(String[] args) {
         Logger.getLogger().setLogLevel(2);
-//        Hero hero = new Hero(3,3,"Hero1");
-        UI ui = new UI(System.out, System.in);
+        ui = new UI(System.out, System.in);
         WebClient cl = new WebClient("http://hoiu3.crtech.space", "HOIU3U");
-//        cl.checkIfMyMove();
-        // // // //cl.uploadString("{\"gameState\": 0,\"currentPlayer\": 1,\"totalPlayers\": 3,\"field\": {\"height\":40,\"width\":40,\"data\": \"00101024025291230242\"}}");
         SessionData gs = cl.fetchGameState();
-//        gs.readField()
-//        Game.gameObjects.add(hero);
         ui.drawField(gs.field);
     }
 
